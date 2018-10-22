edc041fa1ebe93f1df77c801c4da0dca0945b1cb
David Mas <dmas@magento.com>
2018-10-22 12:08:04 +0200

    Use real log operation on context. Use systemrules to simulate the clearmdc flag. Do not reload context, just reset the mockendpoints.

--- src/test/java/poc/mdc/RoutestTest.java
+++ src/test/java/poc/mdc/RoutestTest.java
@@ -1,23 +1,31 @@
 package poc.mdc;
 
-import java.util.Arrays;
+import org.apache.camel.CamelContext;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.FluentProducerTemplate;
 import org.apache.camel.Produce;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.spring.CamelSpringRunner;
 import org.apache.camel.test.spring.CamelTestContextBootstrapper;
+import org.junit.After;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.EnvironmentVariables;
 import org.junit.runner.RunWith;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.BootstrapWith;
 import org.springframework.test.context.ContextConfiguration;
+import java.util.Arrays;
 
 @RunWith(CamelSpringRunner.class)
 @BootstrapWith(CamelTestContextBootstrapper.class)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+// @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @ContextConfiguration(locations = {"classpath*:META-INF/spring/camel-context.xml"})
 public class RoutestTest {
+
+  @Rule
+  public final EnvironmentVariables environmentVariables =
+      new EnvironmentVariables().set("CLEAR_MDC", "true");
 
   @Produce(uri = "direct:in-route")
   private FluentProducerTemplate inRouteProducer;
@@ -33,6 +41,14 @@ public class RoutestTest {
 
   @EndpointInject(uri = "mock:end-of-split")
   private MockEndpoint mockEndOfSplitEndpoint;
+
+  @Autowired
+  private CamelContext camelContext;
+
+  @After
+  public void resetMockEndpoints() {
+    MockEndpoint.resetMocks(camelContext);
+  }
 
   @Test
   public void endOfInRouteShouldReceiveAnEnrichedMessage() throws Exception {
